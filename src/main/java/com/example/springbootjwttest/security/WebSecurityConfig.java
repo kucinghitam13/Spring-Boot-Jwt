dd01c6f82f1c6a5819dbf52d38737301fbbfab7a
Dika Adhitama <dika.adhitama@gmail.com>
2019-07-16 10:58:17 +0700

    Create AuthenticationManager bean

--- src/main/java/com/example/springbootjwttest/security/WebSecurityConfig.java
+++ src/main/java/com/example/springbootjwttest/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springbootjwttest.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -76,5 +77,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public PasswordEncoder passwordEncoder() {
     	return new BCryptPasswordEncoder(12);
+    }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+    	return super.authenticationManagerBean();
     }
 }
